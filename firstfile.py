b57457cf9308bb4493eb7ec4d4a551c42beba386
rahul-office <rahulshriwas.5555@gmail.com>
2024-09-18 12:58:51 +0530

    Add files via upload

---
+++ firstfile.py
@@ -0,0 +1,153 @@
+Python #3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+#Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: C:\Users\HP\Desktop\python code\firstfile.py ============
+segment 1
+segment 2
+segment 3
+welcome dear students
+this is our python class
+
+============= RESTART: C:\Users\HP\Desktop\python code\firstfile.py ============
+segment 1
+segment 2
+segment 3
+welcome dear students
+this is our python class
+first reprensentation
+second reprensentation
+first reprensentation
+24+56+90
+
+============= RESTART: C:\Users\HP\Desktop\python code\firstfile.py ============
+segment 1
+segment 2
+segment 3
+welcome dear students
+this is our python class
+first reprensentation
+second reprensentation
+first reprensentation
+24+56+90
+245690
+
+============= RESTART: C:\Users\HP\Desktop\python code\firstfile.py ============
+segment 1
+segment 2
+segment 3
+welcome dear students
+this is our python class
+first reprensentation
+second reprensentation
+first reprensentation
+24+56+90
+245690
+welcome
+dear
+students
+
+============= RESTART: C:\Users\HP\Desktop\python code\firstfile.py ============
+segment 1
+segment 2
+segment 3
+welcome dear students
+this is our python class
+first reprensentation
+second reprensentation
+first reprensentation
+24+56+90
+245690
+welcome
+dear
+nstudents
+
+welcome
+dear
+students
+
+============= RESTART: C:\Users\HP\Desktop\python code\firstfile.py ============
+segment 1
+segment 2
+segment 3
+welcome dear students
+this is our python class
+first reprensentation
+second reprensentation
+first reprensentation
+24+56+90
+245690
+welcome
+dear
+nstudents
+
+welcome
+dear
+students
+Roll		sub	date
+
+============= RESTART: C:\Users\HP\Desktop\python code\firstfile.py ============
+segment 1
+segment 2
+segment 3
+welcome dear students
+this is our python class
+first reprensentation
+second reprensentation
+first reprensentation
+24+56+90
+245690
+welcome
+dear
+nstudents
+
+welcome
+dear
+students
+Roll		sub	date
+102	english	12/12/2024
+
+============= RESTART: C:\Users\HP\Desktop\python code\firstfile.py ============
+segment 1
+segment 2
+segment 3
+welcome dear students
+this is our python class
+first reprensentation
+second reprensentation
+first reprensentation
+24+56+90
+245690
+welcome
+dear
+nstudents
+
+welcome
+dear
+students
+Roll		sub	date
+102	english		12/12/2024
+>>> 
+============= RESTART: C:\Users\HP\Desktop\python code\firstfile.py ============
+segment 1
+segment 2
+segment 3
+welcome dear students
+this is our python class
+first reprensentation
+second reprensentation
+first reprensentation
+24+56+90
+245690
+welcome
+dear
+nstudents
+
+welcome
+dear
+students
+Roll		sub	date
+102	english		12/12/2024
+102	hindi		12/12/2024
+
+print("welcome")
+
---
+++ variable file.py
@@ -0,0 +1,9 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============= RESTART: C:/Users/HP/Desktop/python code/variable.py =============
+190
+140714979038424
+140714979038808
+140714979038744 140714979038744
+>>> 
